c45274b055b379b50fc75323eff82b921dc44be2
Eddy Dian Turpo Nunez <eddy.turpo@hotmail.it>
2024-09-30 12:27:36 +0200

    creazione ReportMensile

---
+++ src/main/java/com/example/progettoFinale/entities/ReportMensile.java
@@ -0,0 +1,26 @@
+package com.example.progettoFinale.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+public class ReportMensile {
+    private UUID dipendenteId;
+    private String nomeDipendente;
+    private String mese;
+    private Long giorniLavorati;
+    private Long giorniAssenti;
+    private Long ferieRichieste;
+
+    public ReportMensile(UUID dipendenteId, String nomeDipendente, String mese, Long giorniLavorati, Long giorniAssenti, Long ferieRichieste) {
+        this.dipendenteId = dipendenteId;
+        this.nomeDipendente = nomeDipendente;
+        this.mese = mese;
+        this.giorniLavorati = giorniLavorati;
+        this.giorniAssenti = giorniAssenti;
+        this.ferieRichieste = ferieRichieste;
+    }
+}
