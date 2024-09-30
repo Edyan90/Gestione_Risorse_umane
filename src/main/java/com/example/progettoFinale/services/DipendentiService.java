f69b00de53fb14a50b6a763c2192d64415eab92c
Eddy Dian Turpo Nunez <eddy.turpo@hotmail.it>
2024-09-30 11:43:27 +0200

    creazione metodi per update avatar , dipendente e delete dipendente

--- src/main/java/com/example/progettoFinale/services/DipendentiService.java
+++ src/main/java/com/example/progettoFinale/services/DipendentiService.java
@@ -1,5 +1,7 @@
 package com.example.progettoFinale.services;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import com.example.progettoFinale.entities.Dipendente;
 import com.example.progettoFinale.enums.RuoloType;
 import com.example.progettoFinale.exceptions.BadRequestEx;
@@ -14,7 +16,9 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.UUID;
 
 @Service
@@ -25,15 +29,20 @@ public class DipendentiService {
     private PasswordEncoder bcrypt;
     @Autowired
     private MailgunSender mailgunSender;
+    @Autowired
+    private Cloudinary cloudinary;
 
     public Page<Dipendente> findAll(int page, int size, String sortBy){
         if (page>10) page =10;
         Pageable pageable= PageRequest.of(page,size, Sort.by(sortBy));
         return this.dipendentiRepository.findAll(pageable);
     }
+
+
      public Dipendente findByID(UUID id){
         return this.dipendentiRepository.findById(id).orElseThrow(()->new NotFoundEx(id));
      }
+
 
      public Dipendente saveDipendente (DipendenteDTO dipendenteDTO){
         this.dipendentiRepository.findByEmail(dipendenteDTO.email()).ifPresent(dipendente -> {
@@ -62,9 +71,12 @@ public class DipendentiService {
                  throw new BadRequestEx("Stato non valido: " + dipendenteDTO.ruolo() +
                          ". I valori validi sono: DIPENDENTE E MANAGER.");
          }
+         dipendente.setAvatar("https://ui-avatars.com/api/?name=" + dipendenteDTO.nome() + "+" + dipendenteDTO.cognome());
          this.dipendentiRepository.save(dipendente);
          return dipendente;
      }
+
+
      public Dipendente saveNewAdmin(UUID dipendenteID){
         Dipendente dipendente= this.findByID(dipendenteID);
         dipendente.setRuolo(RuoloType.ADMIN);
@@ -72,4 +84,48 @@ public class DipendentiService {
         return dipendente;
      }
 
+    public void findAndDelete(UUID utenteID) {
+        Dipendente dipendente = this.findByID(utenteID);
+        this.dipendentiRepository.delete(dipendente);
+    }
+
+    public Dipendente updateDipendente(UUID dipendenteID,DipendenteDTO dipendenteDTO){
+        Dipendente found=this.findByID(dipendenteID);
+        this.dipendentiRepository.findByEmail(dipendenteDTO.email()).ifPresent(dipendente -> {
+            if (!dipendente.getEmail().equals(dipendenteDTO.email())){
+                throw new BadRequestEx("L'email " + dipendenteDTO.email() + " è già in uso!");
+            }
+        });
+        found.setNome(dipendenteDTO.nome());
+        found.setNome(dipendenteDTO.cognome());
+        found.setEmail(dipendenteDTO.email());
+        found.setNome(dipendenteDTO.stipendio());
+        found.setNome(dipendenteDTO.username());
+        found.setNome(dipendenteDTO.password());
+        switch (dipendenteDTO.ruolo().toLowerCase()) {
+            case "dipendente":
+                found.setRuolo(RuoloType.DIPENDENTE);
+                break;
+            case "manager":
+                found.setRuolo(RuoloType.MANAGER);
+                break;
+
+            default:
+                throw new BadRequestEx("Stato non valido: " + dipendenteDTO.ruolo() +
+                        ". I valori validi sono: DIPENDENTE E MANAGER.");
+        }
+        return this.dipendentiRepository.save(found);
+    }
+
+
+    public Dipendente avatarUpload(UUID dipendenteID, MultipartFile file) {
+        try {
+            String url = (String) cloudinary.uploader().upload(file.getBytes(), ObjectUtils.emptyMap()).get("url");
+            Dipendente dipendente = this.findByID(dipendenteID);
+            dipendente.setAvatar(url);
+            return this.dipendentiRepository.save(dipendente);
+        } catch (IOException e) {
+            throw new BadRequestEx("Errore nel caricamento del file. Verifica il formato e le dimensioni!");
+        }
+    }
 }
