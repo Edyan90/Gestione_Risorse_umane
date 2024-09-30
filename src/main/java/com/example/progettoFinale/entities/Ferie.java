07483d34ae45ab68bb14e2a5c17305bb685a6e5b
Eddy Dian Turpo Nunez <eddy.turpo@hotmail.it>
2024-09-30 12:27:05 +0200

    update Ferie

--- src/main/java/com/example/progettoFinale/entities/Ferie.java
+++ src/main/java/com/example/progettoFinale/entities/Ferie.java
@@ -2,11 +2,14 @@ package com.example.progettoFinale.entities;
 
 import com.example.progettoFinale.enums.StatoFerie;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.UUID;
+
 @Entity
 @Getter
 @Setter
@@ -15,8 +18,8 @@ import java.time.LocalDate;
 public class Ferie {
     @Id
     @GeneratedValue
-    private Long id;
-
+    @Setter(AccessLevel.NONE)
+    private UUID id;
     @ManyToOne
     @JoinColumn(name = "dipendente_id")
     private Dipendente dipendente;
@@ -25,4 +28,11 @@ public class Ferie {
     private LocalDate dataFine;
     @Enumerated(EnumType.STRING)
     private StatoFerie stato;
+
+    public Ferie(Dipendente dipendente, LocalDate dataInizio, LocalDate dataFine, StatoFerie stato) {
+        this.dipendente = dipendente;
+        this.dataInizio = dataInizio;
+        this.dataFine = dataFine;
+        this.stato = stato;
+    }
 }
