93a75b9d217f8bec5f744b075e38a35f43f91115
Eddy Dian Turpo Nunez <eddy.turpo@hotmail.it>
2024-09-30 11:11:43 +0200

    creazione DipendentDTO

---
+++ src/main/java/com/example/progettoFinale/recordsDTO/DipendenteDTO.java
@@ -0,0 +1,30 @@
+package com.example.progettoFinale.recordsDTO;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+public record DipendenteDTO(
+        @NotEmpty(message = "Il nome è obbligatorio!")
+        @Size(min = 3, max = 30, message = "Il nome deve avere un minimo di 2 ad un massimo di 20 caratteri")
+        String nome,
+        @NotEmpty(message = "Il cognome è obbligatorio!")
+        @Size(min = 3, max = 30, message = "Il cognome deve avere un minimo di 2 ad un massimo di 20 caratteri")
+        String cognome,
+        @Email
+        @NotEmpty(message = "l'email è obbligatorio!")
+        @Size(min = 3, max = 30, message = "l'email deve avere un minimo di 2 ad un massimo di 20 caratteri")
+        String email,
+        @NotEmpty(message = "L'username username è obbligatorio!")
+        @Size(min = 3, max = 30, message = "Il cognome deve avere un minimo di 2 ad un massimo di 20 caratteri")
+        String username,
+        @NotEmpty(message = "La password è obbligatoria!")
+        @Size(min = 3, max = 30, message = "La password deve avere un minimo di 2 ad un massimo di 20 caratteri")
+        String password,
+        @NotEmpty(message = "Lo stipendio è obbligatoria!")
+        String stipendio,
+        @NotEmpty(message = "il ruolo è obbligatorio!")
+        @Size(min = 3, max = 30, message = "i ruoli disponibili sono DIPENDENTE,MANAGER")
+        String ruolo
+) {
+}
