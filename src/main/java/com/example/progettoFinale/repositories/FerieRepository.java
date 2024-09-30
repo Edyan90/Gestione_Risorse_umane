9a8c6a271b3d9b087dc89b35b705aea6b27d7b92
Eddy Dian Turpo Nunez <eddy.turpo@hotmail.it>
2024-09-30 12:27:48 +0200

    fix bug

--- src/main/java/com/example/progettoFinale/repositories/FerieRepository.java
+++ src/main/java/com/example/progettoFinale/repositories/FerieRepository.java
@@ -1,9 +1,10 @@
 package com.example.progettoFinale.repositories;
 
+import com.example.progettoFinale.entities.Ferie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 @Repository
-public interface FerieRepository extends JpaRepository<FerieRepository, UUID> {
+public interface FerieRepository extends JpaRepository<Ferie, UUID> {
 }
