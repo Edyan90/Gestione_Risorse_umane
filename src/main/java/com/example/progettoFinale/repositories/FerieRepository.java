0af76a1f4f9578e304846322076783a5cced60ec
Eddy Dian Turpo Nunez <eddy.turpo@hotmail.it>
2024-10-01 18:58:13 +0200

    creazione metodo per avere una lista di ferie per ogni dipendente

--- src/main/java/com/example/progettoFinale/repositories/FerieRepository.java
+++ src/main/java/com/example/progettoFinale/repositories/FerieRepository.java
@@ -4,7 +4,11 @@ import com.example.progettoFinale.entities.Ferie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
+
 @Repository
 public interface FerieRepository extends JpaRepository<Ferie, UUID> {
+
+    List<Ferie> findByDipendenteId(UUID dipendenteId);
 }
