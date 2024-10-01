60906cc8b17bbc50cbd46c83b03e033fda199c5b
Eddy Dian Turpo Nunez <eddy.turpo@hotmail.it>
2024-10-01 18:58:44 +0200

    aggiunto metodo per aggiornare le ferie

--- src/main/java/com/example/progettoFinale/services/FerieService.java
+++ src/main/java/com/example/progettoFinale/services/FerieService.java
@@ -4,8 +4,11 @@ package com.example.progettoFinale.services;
 import com.example.progettoFinale.entities.Dipendente;
 import com.example.progettoFinale.entities.Ferie;
 import com.example.progettoFinale.enums.StatoFerie;
+import com.example.progettoFinale.exceptions.BadRequestEx;
 import com.example.progettoFinale.exceptions.NotFoundEx;
+import com.example.progettoFinale.recordsDTO.FerieApprovazioneDTO;
 import com.example.progettoFinale.recordsDTO.FerieDTO;
+import com.example.progettoFinale.recordsDTO.FerieRespDTO;
 import com.example.progettoFinale.repositories.FerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -14,6 +17,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.awt.font.TextHitInfo;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -52,5 +57,25 @@ public class FerieService {
         this.ferieRepository.save(found);
         return found;
     }
-    
+
+    public List<Ferie> ferieDelDipendente(UUID dipendenteID) {
+        Dipendente dipendente = this.dipendentiService.findByID(dipendenteID);
+        return this.ferieRepository.findByDipendenteId(dipendente.getId());
+    }
+
+    public FerieRespDTO approvazioneFerie(UUID ferieID, FerieApprovazioneDTO ferieApprovazioneDTO) {
+        Ferie found = this.findByID(ferieID);
+        switch (ferieApprovazioneDTO.approvazione().toLowerCase()) {
+            case "approvato":
+                found.setStato(StatoFerie.APPROVATO);
+                break;
+            case "rifiutato":
+                found.setStato(StatoFerie.RIFIUTATO);
+                break;
+            default:
+                throw new BadRequestEx("Stato non valido: "+ferieApprovazioneDTO.approvazione()+". I valori validi sono APPROVATO O RIFIUTATO.")
+        }
+        return new FerieRespDTO(String.valueOf(this.ferieRepository.save(found)));
+
+    }
 }
