72e21ccf65a52c5ada28091016ac5bd1d4388146
Eddy Dian Turpo Nunez <eddy.turpo@hotmail.it>
2024-10-02 11:40:29 +0200

    Create FerieListDateStatoDTO.java

---
+++ src/main/java/com/example/progettoFinale/recordsDTO/FerieListDateStatoDTO.java
@@ -0,0 +1,15 @@
+package com.example.progettoFinale.recordsDTO;
+
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+
+public record FerieListDateStatoDTO(
+        @NotNull(message = "manca la data d'inizio ferie!")
+        LocalDate dataInizio,
+        @NotNull(message = "manca la data di fine ferie!")
+        LocalDate dataFine,
+        @NotNull(message = "manca lo stato delle ferie se APPROVATO,RIFIUTATO O RICHIESTO!")
+        String stato
+) {
+}
