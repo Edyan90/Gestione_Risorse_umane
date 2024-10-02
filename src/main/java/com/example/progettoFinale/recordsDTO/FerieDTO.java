e6b13c0f0473761efeda2f15c41df447de467f9e
Eddy Dian Turpo Nunez <eddy.turpo@hotmail.it>
2024-10-02 17:13:15 +0200

    Update FerieDTO.java

--- src/main/java/com/example/progettoFinale/recordsDTO/FerieDTO.java
+++ src/main/java/com/example/progettoFinale/recordsDTO/FerieDTO.java
@@ -1,17 +1,15 @@
 package com.example.progettoFinale.recordsDTO;
 
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-
 
 import java.time.LocalDate;
 
-public record FerieDTO (
-@NotEmpty(message = "l'id del dipendente è obbligatorio!")
-String dipendenteID,
-@NotNull(message = "manca la data d'inizio ferie!")
-LocalDate dataInizio,
-@NotNull(message = "manca la data di fine ferie!")
-LocalDate dataFine
-){
+public record FerieDTO(
+//@NotEmpty(message = "l'id del dipendente è obbligatorio!")
+//String dipendenteID,
+        @NotNull(message = "manca la data d'inizio ferie!")
+        LocalDate dataInizio,
+        @NotNull(message = "manca la data di fine ferie!")
+        LocalDate dataFine
+) {
 }
