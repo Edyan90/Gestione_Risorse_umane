e96107df8d2f1d50e8635dad8f8dd124aa224ea9
Eddy Dian Turpo Nunez <eddy.turpo@hotmail.it>
2024-10-02 11:40:51 +0200

    creati metodi per dei filtri su FerieRepository

--- src/main/java/com/example/progettoFinale/repositories/FerieRepository.java
+++ src/main/java/com/example/progettoFinale/repositories/FerieRepository.java
@@ -1,9 +1,12 @@
 package com.example.progettoFinale.repositories;
 
 import com.example.progettoFinale.entities.Ferie;
+import com.example.progettoFinale.enums.StatoFerie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
@@ -11,4 +14,9 @@ import java.util.UUID;
 public interface FerieRepository extends JpaRepository<Ferie, UUID> {
 
     List<Ferie> findByDipendenteId(UUID dipendenteId);
+
+    List<Ferie> findByStato(StatoFerie stato);
+
+    @Query("SELECT f FROM Ferie f WHERE f.stato = :stato AND f.dataInizio >= :data1 AND f.dataFine <= :data2")
+    List<Ferie> findByDateRangeAndStato(LocalDate data1, LocalDate data2, StatoFerie stato);
 }
