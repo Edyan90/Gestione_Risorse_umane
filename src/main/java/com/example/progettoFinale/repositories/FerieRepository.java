ed49a7aa3b82ddb8a8cb2647b2f8d7cda310b55c
Eddy Dian Turpo Nunez <eddy.turpo@hotmail.it>
2024-10-02 17:13:33 +0200

    aggiunti filtri per il service di Ferie

--- src/main/java/com/example/progettoFinale/repositories/FerieRepository.java
+++ src/main/java/com/example/progettoFinale/repositories/FerieRepository.java
@@ -1,9 +1,11 @@
 package com.example.progettoFinale.repositories;
 
+import com.example.progettoFinale.entities.Dipendente;
 import com.example.progettoFinale.entities.Ferie;
 import com.example.progettoFinale.enums.StatoFerie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -19,4 +21,12 @@ public interface FerieRepository extends JpaRepository<Ferie, UUID> {
 
     @Query("SELECT f FROM Ferie f WHERE f.stato = :stato AND f.dataInizio >= :data1 AND f.dataFine <= :data2")
     List<Ferie> findByDateRangeAndStato(LocalDate data1, LocalDate data2, StatoFerie stato);
+
+    @Query("SELECT f FROM Ferie f WHERE f.dipendente=:dipendente AND f.stato <> 'RICHIESTO'")
+    List<Ferie> findByStoricoFerie(@Param("dipendente") Dipendente dipendente);
+
+    @Query("SELECT f FROM Ferie f WHERE f.dipendente = :dipendente AND " +
+            "(f.dataInizio <= :dataFine AND f.dataFine >= :dataInizio)")
+    List<Ferie> findFerieSovrapposte(Dipendente dipendente, LocalDate dataInizio, LocalDate dataFine);
+
 }
