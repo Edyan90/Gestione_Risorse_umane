b6e62e2f3f482adfd28fe6056681af4722461f4d
Eddy Dian Turpo Nunez <eddy.turpo@hotmail.it>
2024-10-03 13:08:30 +0200

    Update PresenzaRepository.java

--- src/main/java/com/example/progettoFinale/repositories/PresenzaRepository.java
+++ src/main/java/com/example/progettoFinale/repositories/PresenzaRepository.java
@@ -1,10 +1,22 @@
 package com.example.progettoFinale.repositories;
 
+import com.example.progettoFinale.entities.Dipendente;
 import com.example.progettoFinale.entities.Presenza;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
+
 @Repository
 public interface PresenzaRepository extends JpaRepository<Presenza, UUID> {
+    List<Presenza> findByDipendentId(UUID dipendenteId);
+
+    @Query("SELECT p FROM Presenza p WHERE p.dipendente = :dipendente AND MONTH(p.data) = :mese AND YEAR(p.data) = :anno")
+    List<Presenza> findByStoricoPresenzeMensile(@Param("dipendente") Dipendente dipendente,
+                                                @Param("mese") int mese,
+                                                @Param("anno") int anno);
+
 }
