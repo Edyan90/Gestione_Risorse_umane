0f7e0d1412d26a13336a9e1a96ffd10c321c1006
Eddy Dian Turpo Nunez <eddy.turpo@hotmail.it>
2024-10-03 13:08:44 +0200

    creati DTO per presenza

---
+++ src/main/java/com/example/progettoFinale/recordsDTO/PresenzaDTO.java
@@ -0,0 +1,16 @@
+package com.example.progettoFinale.recordsDTO;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+
+public record PresenzaDTO(
+        @NotNull(message = "La data è obbligatoria in formato YYYY-MM-DD!")
+        LocalDate data,
+        @NotEmpty(message = "la presenza è obbligatoria in TRUE o FALSE")
+        Boolean presente,
+        @NotEmpty(message = "Il dipendenteID è obbligatorio")
+        String dipendenteID
+) {
+}
---
+++ src/main/java/com/example/progettoFinale/recordsDTO/PresenzaRespDTO.java
@@ -0,0 +1,8 @@
+package com.example.progettoFinale.recordsDTO;
+
+import java.util.UUID;
+
+public record PresenzaRespDTO(
+        String message, UUID presenzaID
+) {
+}
